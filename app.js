eaaa2d3fd0a779b1ec204968b6feddfc6c87067b
shri124 <spate615@uic.edu>
2024-04-22 13:52:22 -0500

    added querying and env files

--- app.js
+++ app.js
@@ -10,34 +10,34 @@ const client = new Client({
 })
 
 
-client.connect()
-.then(() => console.log("Connected successfully"))
-.then(() => client.query("select * from Document"))
-.then(results => console.table(results.rows))
-.catch(e => console.log)
-.finally(() => client.end())
-
-
 // client.connect()
-// .then(() => {
-//   console.log("Connected successfully");
-//   return client.query('SELECT * FROM document'); // Notice the double quotes
-// })
-// .then(results => {
-//   console.log("Query executed, processing results...");
-//   if (results.rows.length > 0) {
-//     console.table(results.rows);
-//   } else {
-//     console.log("No data found.");
-//   }
-// })
-// .catch(e => {
-//   console.error("An error occurred:", e);
-// })
-// .finally(() => {
-//   console.log("Closing the database connection.");
-//   client.end();
-// });
+// .then(() => console.log("Connected successfully"))
+// .then(() => client.query("select * from Document"))
+// .then(results => console.table(results.rows))
+// .catch(e => console.log)
+// .finally(() => client.end())
 
 
+client.connect()
+.then(() => {
+  console.log("Connected successfully");
+  return client.query('SELECT * FROM document'); // Notice the double quotes
+})
+.then(results => {
+  console.log("Query executed, processing results...");
+  if (results.rows.length > 0) {
+    console.table(results.rows);
+  } else {
+    console.log("No data found.");
+  }
+})
+.catch(e => {
+  console.error("An error occurred:", e);
+})
+.finally(() => {
+  console.log("Closing the database connection.");
+  client.end();
+});
+
+module.exports = client
 
