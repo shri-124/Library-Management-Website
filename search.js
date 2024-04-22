484524e007b11bbe74474c5d8f6f324336351ccf
sohumbhole <sohumbhole@gmail.com>
2024-04-21 23:51:07 -0500

    Updated Client dashboard. Now has search bar that connects to JS function

---
+++ search.js
@@ -0,0 +1,5 @@
+function submitSearch() {
+    var searchInput = document.getElementById('search-input');
+    alert('Search query: ' + searchInput.value);
+    // Additional actions can be performed with searchInput.value here
+}
